e2e0e8b69c7243b00d506b8e678f6a647a57e75c
Saurabh7762 <Saurabhkumars7762@gmail.com>
2023-02-22 22:19:30 +0530

    Basic

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
         path: path.join(__dirname, "/dist"),
         filename: "index_bundle.js",
     },
+    devServer: {
+        historyApiFallback: true
+        
+    },  
     module: {
         rules: [
             {
